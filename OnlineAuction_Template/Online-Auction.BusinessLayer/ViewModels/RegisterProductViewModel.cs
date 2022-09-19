111d0a9a3c1794adee5efa77118bf8f84a7ea800
sanahaju123 <88725494+sanahaju123@users.noreply.github.com>
2022-09-19 11:38:33 +0530

    Updated

--- OnlineAuction_Template/Online-Auction.BusinessLayer/ViewModels/RegisterProductViewModel.cs
+++ OnlineAuction_Template/Online-Auction.BusinessLayer/ViewModels/RegisterProductViewModel.cs
@@ -10,29 +10,24 @@ namespace Online_Auction.BusinessLayer.ViewModels
 {
     public class RegisterProductViewModel
     {
-        [Key]
         public long ProductId { get; set; }
 
-        [Required]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Maximum 100 characters")]
         public string Name { get; set; }
 
-        [Required]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Maximum 100 characters")]
         public string Description { get; set; }
+        
         public int Quantity { get; set; }
+        
         public decimal StartBiddingAmount { get; set; }
+        
         public decimal Price { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BiddingLastDate { get; set; }
 
-        [Required]
         public long CategoryId { get; set; }
-        [Required]
+      
         public long SellerId { get; set; }
 
-        [DefaultValue(false)]
         public bool IsDeleted { get; set; }
     }
 }
